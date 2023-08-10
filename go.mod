7d9883dc9c761a435025b3bbae33d89dca284b2c
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2023-08-09 22:06:28 -0700

    checkpoint

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/jsha/ctile
 
 go 1.20
+
+require (
+	github.com/aws/aws-sdk-go v1.44.320 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,18 +1,28 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"strconv"
 	"strings"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
 )
 
 const backend = "https://oak.ct.letsencrypt.org/2023"
 const tileSize = 256
+const s3bucket = "ctile"
 
 func main() {
+	sess := session.Must(session.NewSession())
+	svc := s3.New(sess)
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		if !strings.HasSuffix(r.URL.Path, "/ct/v1/get-entries") {
 			w.WriteHeader(http.StatusNotFound)
@@ -45,9 +55,9 @@ func main() {
 		}
 		fmt.Printf("%d\n", endInt) // XXX
 
-		tileStart := startInt / tileSize
-		tileEnd := tileStart + tileSize
 		tileOffset := startInt % tileSize
+		tileStart := startInt - tileOffset
+		tileEnd := tileStart + tileSize
 
 		url := fmt.Sprintf("%s/%s?start=%d&end=%d", backend, r.URL.Path, tileStart, tileEnd)
 		resp, err := http.Get(url)
@@ -76,9 +86,22 @@ func main() {
 			fmt.Fprintf(w, "reading body from %s: %s", url, err)
 		}
 
-		if len(entries.Entries) != tileSize {
+		if len(entries.Entries) >= tileSize {
 			w.WriteHeader(http.StatusInternalServerError)
 			fmt.Fprintf(w, "expected %d entries, got %d", tileSize, len(entries.Entries))
+		}
+
+		if len(entries.Entries) == tileSize {
+			key := fmt.Sprintf("%d", tileStart)
+			ctx := context.TODO()
+			_, err := svc.PutObjectWithContext(ctx, &s3.PutObjectInput{
+				Bucket: aws.String(s3bucket),
+				Key:    aws.String(key),
+				Body:   os.Stdin,
+			})
+			if err != nil {
+				log.Printf("putting in s3 bucket %q with key %q: %s", s3bucket, key, err)
+			}
 		}
 
 		// Truncate to match the request
