6d33b7357148d76c7a4ce432c02b1bcb2bc98104
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2023-09-06 08:31:39 -0700

    Make entries immutable (#18)
    This fixes a race condition when concurrent requests share a
    singleflight response. Previously we mutated the `contents.Entries`
    field to adjust for the user's actual requested range. This is always a
    race condition; sometimes it also resulted in a panic (index out of
    range), when one goroutine shortened the slice and another tried to
    access it.

---
+++ main_test.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestTrimForDisplay(t *testing.T) {
+	entries := &entries{
+		Entries: []entry{
+			{},
+			{},
+			{},
+		},
+	}
+	_, err := entries.trimForDisplay(1, 2, tile{start: 10, end: 20, size: 10, logURL: "http://example.com"})
+	if err == nil {
+		t.Fatal("expected error, got none")
+	}
+	if !strings.Contains(err.Error(), "internal inconsistency") {
+		t.Errorf("expected internal inconsistency error, got %s", err)
+	}
+
+	_, err = entries.trimForDisplay(999, 1000, tile{start: 10, end: 20, size: 10, logURL: "http://example.com"})
+	if err == nil {
+		t.Fatal("expected error, got none")
+	}
+	if !strings.Contains(err.Error(), "internal inconsistency") {
+		t.Errorf("expected internal inconsistency error, got %s", err)
+	}
+
+	_, err = entries.trimForDisplay(1000, 1000, tile{start: 10, end: 20, size: 10, logURL: "http://example.com"})
+	if err == nil {
+		t.Fatal("expected error, got none")
+	}
+	if !strings.Contains(err.Error(), "internal inconsistency") {
+		t.Errorf("expected internal inconsistency error, got %s", err)
+	}
+
+	_, err = entries.trimForDisplay(10, 20, tile{start: 10, end: 12, size: 2, logURL: "http://example.com"})
+	if err == nil {
+		t.Fatal("expected error, got none")
+	}
+	if !strings.Contains(err.Error(), "internal inconsistency") {
+		t.Errorf("expected internal inconsistency error, got %s", err)
+	}
+
+	_, err = entries.trimForDisplay(15, 20, tile{start: 10, end: 20, size: 10, logURL: "http://example.com"})
+	if err == nil {
+		t.Fatal("expected error, got none")
+	}
+	if !strings.Contains(err.Error(), "past the end of the log") {
+		t.Errorf("expected 'past the end of the log' error, got %s", err)
+	}
+
+	e, err := entries.trimForDisplay(10, 20, tile{start: 10, end: 20, size: 10, logURL: "http://example.com"})
+	if err != nil {
+		t.Fatalf("expected success, got %s", err)
+	}
+	if len(e.Entries) != 3 {
+		t.Errorf("expected 3 entries got %d", len(entries.Entries))
+	}
+
+	e, err = entries.trimForDisplay(11, 12, tile{start: 10, end: 20, size: 10, logURL: "http://example.com"})
+	if err != nil {
+		t.Fatalf("expected success, got %s", err)
+	}
+	if len(e.Entries) != 1 {
+		t.Errorf("expected 1 entry got %d", len(entries.Entries))
+	}
+
+	e, err = entries.trimForDisplay(12, 20, tile{start: 10, end: 20, size: 10, logURL: "http://example.com"})
+	if err != nil {
+		t.Fatalf("expected success, got %s", err)
+	}
+	if len(e.Entries) != 1 {
+		t.Errorf("expected 1 entry got %d", len(entries.Entries))
+	}
+}
